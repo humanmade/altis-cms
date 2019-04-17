a2174d9d84b1989da3b273b361c03322b0ceeb9d
Joe Hoyle <joehoyle@gmail.com>
2019-04-17 11:25:21 +0700

    Add WP User Signups
    Fixes #29.

--- inc/namespace.php
+++ inc/namespace.php
@@ -3,6 +3,7 @@
 namespace HM\Platform\CMS;
 
 use function HM\Platform\get_config;
+use const HM\Platform\ROOT_DIR;
 
 /**
  * Main bootstrap / entry point for the CMS module.
@@ -18,6 +19,8 @@ function bootstrap() {
 	if ( $config['login-logo'] ) {
 		add_action( 'login_header', __NAMESPACE__ . '\\add_login_logo' );
 	}
+
+	add_action( 'plugins_loaded', __NAMESPACE__ . '\\load_plugins', 1 );
 }
 
 /**
@@ -34,4 +37,11 @@ function add_login_logo() {
 		}
 	</style>
 	<?php
+}
+
+/**
+ * Load plugins that are bundled with the CMS module.
+ */
+function load_plugins() {
+	require_once ROOT_DIR . '/vendor/stuttter/wp-user-signups/wp-user-signups.php';
 }
