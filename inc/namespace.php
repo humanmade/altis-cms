aad475d643d739bcae59b30b4b2f93e33dde86fe
Joe Hoyle <joehoyle@gmail.com>
2019-04-17 11:17:28 +0700

    Disable the file editor
    Fixes #26.

--- inc/namespace.php
+++ inc/namespace.php
@@ -18,6 +18,10 @@ function bootstrap() {
 	if ( $config['login-logo'] ) {
 		add_action( 'login_header', __NAMESPACE__ . '\\add_login_logo' );
 	}
+
+	if ( ! defined( 'DISALLOW_FILE_EDIT' ) ) {
+		define( 'DISALLOW_FILE_EDIT', true );
+	}
 }
 
 /**
