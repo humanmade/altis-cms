91b17c1de1192b92dae01588fc7393134510472f
Robert O'Rourke <robert@o-rourke.org>
2019-04-18 11:59:12 +0100

    add block categories and docs

--- inc/block_editor/namespace.php
+++ inc/block_editor/namespace.php
@@ -6,8 +6,44 @@ namespace HM\Platform\CMS\Block_Editor;
  * Set up block editor modifications.
  */
 function bootstrap() {
-	add_filter( 'register_post_type_args', __NAMESPACE__ . '\\show_wp_block_in_menu', 10, 2 );
+	add_action( 'init', __NAMESPACE__ . '\\register_block_categories' );
 	add_action( 'admin_menu', __NAMESPACE__ . '\\admin_menu', 9 );
+	add_filter( 'register_post_type_args', __NAMESPACE__ . '\\show_wp_block_in_menu', 10, 2 );
+}
+
+/**
+ * Create the block categories taxonomy.
+ */
+function register_block_categories() {
+	register_taxonomy( 'wp_block_category', 'wp_block', [
+		'label' => __( 'Block Categories', 'hm-platform' ),
+		'labels' => [
+			'name'                       => _x( 'Block Categories', 'taxonomy general name', 'hm-platform' ),
+			'singular_name'              => _x( 'Block Category', 'taxonomy singular name', 'hm-platform' ),
+			'search_items'               => __( 'Search Block Categories', 'hm-platform' ),
+			'popular_items'              => __( 'Popular Block Categories', 'hm-platform' ),
+			'all_items'                  => __( 'All Writers', 'hm-platform' ),
+			'parent_item'                => null,
+			'parent_item_colon'          => null,
+			'edit_item'                  => __( 'Edit Block Category', 'hm-platform' ),
+			'update_item'                => __( 'Update Block Category', 'hm-platform' ),
+			'add_new_item'               => __( 'Add New Block Category', 'hm-platform' ),
+			'new_item_name'              => __( 'New Block Category Name', 'hm-platform' ),
+			'separate_items_with_commas' => __( 'Separate block categories with commas', 'hm-platform' ),
+			'add_or_remove_items'        => __( 'Add or remove block categories', 'hm-platform' ),
+			'choose_from_most_used'      => __( 'Choose from the most used block categories', 'hm-platform' ),
+			'not_found'                  => __( 'No block categories found.', 'hm-platform' ),
+			'menu_name'                  => __( 'Categories', 'hm-platform' ),
+		],
+		'public' => true,
+		'publicly_queryable' => false,
+		'show_ui' => true,
+		'show_in_nav_menus' => false,
+		'show_in_rest' => true,
+		'hierarchical' => true,
+		'show_admin_column' => true,
+		'rewrite' => false,
+	] );
 }
 
 /**
