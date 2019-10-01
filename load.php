7ff87f2007383823cc4c62b002192ae018b4c277
Robert O'Rourke <robert@o-rourke.org>
2019-10-01 13:00:36 +0100

    Support new autoload method

--- load.php
+++ load.php
@@ -4,17 +4,6 @@ namespace Altis\CMS; // @codingStandardsIgnoreLine
 
 use function Altis\register_module;
 
-require_once __DIR__ . '/inc/namespace.php';
-require_once __DIR__ . '/inc/remove_updates/namespace.php';
-require_once __DIR__ . '/inc/branding/namespace.php';
-require_once __DIR__ . '/inc/block_editor/namespace.php';
-require_once __DIR__ . '/inc/permalinks/namespace.php';
-
-// Don't self-initialize if this is not an Altis execution.
-if ( ! function_exists( 'add_action' ) ) {
-	return;
-}
-
 // Register core module.
 add_action( 'altis.modules.init', function () {
 	$default_settings = [
