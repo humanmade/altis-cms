911c1b21c71043da682f85637f0ac0eac1bb2960
Ryan McCue <me@ryanmccue.info>
2019-11-16 18:38:37 +0000

    Use one const per variable

--- assets/customize-settings.js
+++ assets/customize-settings.js
@@ -1,13 +1,12 @@
 /*
  * Custom JS to modify the Altis CMS module Add New Site page
  */
-const siteSubdomain = document.getElementById( 'site-subdomain' ),
-	siteSubdirectory = document.getElementById( 'site-subdirectory' ),
-	siteCustomDomain = document.getElementById( 'site-custom-domain' ),
-	domainTypeRadioBtns = document.getElementsByName( 'domain-type' ),
-	siteAddress = document.getElementById( 'site-address' ),
-	siteAddressDesc = document.getElementById( 'site-address-desc' ),
-	networkAddress = window.location.hostname;
+const siteSubdomain = document.getElementById( 'site-subdomain' );
+const siteSubdirectory = document.getElementById( 'site-subdirectory' );
+const siteCustomDomain = document.getElementById( 'site-custom-domain' );
+const siteAddress = document.getElementById( 'site-address' );
+const siteAddressDesc = document.getElementById( 'site-address-desc' );
+const networkAddress = window.location.hostname;
 let domainTextEl = document.createElement( 'span' );
 domainTextEl.className = 'site-address-domain-text';
 
@@ -40,7 +39,7 @@ function updatedomainText( event ) {
 }
 
 // Add an event listener to each radio button.
-domainTypeRadioBtns.forEach( function ( element ) {
+document.getElementsByName( 'domain-type' ).forEach( function ( element ) {
 	element.addEventListener( 'change', updatedomainText );
 
 	// On load, dispatch a change event for the selected radio button.
