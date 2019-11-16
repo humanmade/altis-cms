32a45f9894abaf5c28693e8347ef143836901aba
Ryan McCue <me@ryanmccue.info>
2019-11-16 18:35:27 +0000

    Revert unrelated changes to branding functionality

--- inc/branding/namespace.php
+++ inc/branding/namespace.php
@@ -17,7 +17,7 @@ const COLOR_OFFWHITE = '#f3f5f9';
  */
 function bootstrap() {
 	add_action( 'add_admin_bar_menus', __NAMESPACE__ . '\\remove_wordpress_admin_bar_item' );
-	add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\enqueue_admin_scripts' );
+	add_action( 'admin_bar_init', __NAMESPACE__ . '\\enqueue_admin_scripts' );
 	add_action( 'admin_bar_menu', __NAMESPACE__ . '\\admin_bar_menu' );
 	add_filter( 'admin_footer_text', '__return_empty_string' );
 	add_action( 'wp_network_dashboard_setup', __NAMESPACE__ . '\\remove_dashboard_widgets' );
@@ -228,7 +228,7 @@ function wordpress_to_altis( string $text ) : string {
  */
 function override_generator( string $gen, string $type ) : string {
 	$wp_version = get_bloginfo( 'version' );
-	$wp_version_rss = convert_chars( wp_strip_all_tags( $wp_version ) );
+	$wp_version_rss = convert_chars( strip_tags( $wp_version ) );
 	switch ( $type ) {
 		case 'html':
 			return sprintf(
