8cfca486ded58cece641fd49e1cfe7fabca63f43
Kevin Langley <me@kevinlangleyjr.com>
2020-01-08 15:42:02 -0500

    Adding extended custom post types library.

--- inc/namespace.php
+++ inc/namespace.php
@@ -128,6 +128,7 @@ function add_login_logo() {
  */
 function load_plugins() {
 	require_once ROOT_DIR . '/vendor/stuttter/wp-user-signups/wp-user-signups.php';
+	require_once ROOT_DIR . '/vendor/johnbillion/extended-cpts/extended-cpts.php';
 }
 
 /**
