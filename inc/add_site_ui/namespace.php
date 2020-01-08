8ae125d8004a878f5b2ae7005e942ebffbc74801
Kevin Langley <me@kevinlangleyjr.com>
2020-01-08 18:01:26 -0500

    Fixing undefined variable notice when including the schema for a custom domain.

--- inc/add_site_ui/namespace.php
+++ inc/add_site_ui/namespace.php
@@ -417,10 +417,10 @@ function handle_subdirectory( string $value ) : ?array {
 /**
  * Handle a custom domain input value.
  *
- * @param string $value Input custom domain value.
+ * @param string $url Input custom domain value.
  * @return array|null $value Assoc array with domain and path keys, or null if invalid.
  */
-function handle_custom_domain( string $value ) : ?array {
+function handle_custom_domain( string $url ) : ?array {
 	if ( strpos( $value, 'http' ) !== 0 ) {
 		$url = 'https://' . $value;
 	}
