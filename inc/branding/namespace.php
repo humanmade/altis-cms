21b28a0a83fef001c787defc4d18e2b6b07eca85
Robert O'Rourke <robert@o-rourke.org>
2020-02-26 15:55:19 +0000

    merge 192

--- inc/branding/namespace.php
+++ inc/branding/namespace.php
@@ -155,7 +155,7 @@ function detect_missing_default_theme() {
 function admin_bar_menu( WP_Admin_Bar $wp_admin_bar ) {
 	$logo_menu_args = [
 		'id'    => 'altis',
-		'title' => '<span class="icon"><img src="' . get_logo_url( 'white' ) . '" /></span>',
+		'title' => '<span class="altis-logo-wrapper"><img src="' . get_logo_url( 'white' ) . '" /></span>',
 	];
 
 	// Set tabindex="0" to make sub menus accessible when no URL is available.
