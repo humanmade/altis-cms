cb90a4db3c9c76508ca120e07860cbbe8280c565
Robert O'Rourke <robert@o-rourke.org>
2020-07-13 11:38:33 +0100

    Apply suggestions from code review

--- inc/real_guids/namespace.php
+++ inc/real_guids/namespace.php
@@ -2,6 +2,9 @@
 
 namespace Altis\CMS\Real_GUIDs;
 
+/**
+ * Add filters for setting real GUIDs.
+ */
 function bootstrap() {
 	add_filter( 'kses_allowed_protocols', __NAMESPACE__ . '\\add_urn_protocol' );
 	add_filter( 'wp_insert_post_data', __NAMESPACE__ . '\\add_uuid_to_new' );
@@ -27,7 +30,7 @@ function add_urn_protocol( array $protocols ) : array {
  * @return array
  */
 function add_uuid_to_new( array $data ) : array {
-	// Set a default GUID
+	// Set a default GUID.
 	if ( empty( $data['guid'] ) ) {
 		$data['guid'] = wp_slash( sprintf( 'urn:uuid:%s', wp_generate_uuid4() ) );
 	}
