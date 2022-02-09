a59030817cf740848b8b59f871c24fad9b2c39be
Shady Sharaf <shady@sharaf.me>
2022-02-09 08:59:42 +0000

    Reindex content after DB changes

--- tests/acceptance/AuthorshipCest.php
+++ tests/acceptance/AuthorshipCest.php
@@ -63,6 +63,8 @@ class AuthorshipCest {
 	public function testAuthorship( AcceptanceTester $I ) {
 		$I->haveUserInDatabase( 'Arthur', 'author' );
 
+		$I->reindexElastic();
+
 		$I->wantToTest( 'I can add multiple authors' );
 		$I->loginAsAdmin();
 
